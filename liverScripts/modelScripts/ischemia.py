117ca2744158662fb142baa0da936f4e18f96afa
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2021-10-25 02:35:14 -0400

    Updated ischemia info

--- liverScripts/modelScripts/ischemia.py
+++ liverScripts/modelScripts/ischemia.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 import equations
 import pc
+import fluxes as fl
 
 J_AtC = 7.0e-4
 glyc = 2.3e-4
@@ -38,41 +39,41 @@ def h(t, y):
 def main(): ## Runs differential equation for time span and outputs results to
     ## a csv file and a feather file.
     count = 0
-    # pc.finalConditions[pc.pcIS.iPYR_c] = normPYR/10.0
-    # pc.finalConditions[pc.pcIS.iO2_x] = normO2/10.0
-    # #print(f(0, pc.finalConditions))
-    #
-    # count = 1
-    # results = sci.solve_ivp(fun = f,
-    #                         t_span = (0, 10000),
-    #                         y0 = pc.finalConditions,
-    #                         method = "LSODA",
-    #                         atol = 1e-10,
-    #                         rtol = 1e-10)
-    #
-    # final = results.y.transpose()[-1]
-    # results = np.concatenate((np.array([results.t]), results.y)).transpose()
-    # results = pd.DataFrame(results,
-    #                        columns = ["t", "H_x", "dPsi", "ATP_x", "ADP_x",
-    #                                   "AMP_x", "GTP_x", "GDP_x", "Pi_x", "NADH_x",
-    #                                   "QH2_x", "OAA_x", "ACCOA_x", "CIT_x", "ICIT_x",
-    #                                   "AKG_x", "SCOA_x", "COASH_x", "SUC_x", "FUM_x",
-    #                                   "MAL_x", "GLU_x", "ASP_x", "K_x", "Mg_x",
-    #                                   "O2_x", "CO2tot_x", "Cred_i", "ATP_i", "ADP_i",
-    #                                   "AMP_i", "Pi_i", "H_i", "Mg_i", "K_i", "ATP_c",
-    #                                   "ADP_c", "Pi_c", "H_c", "Mg_c", "K_c", "PYR_x",
-    #                                   "PYR_i", "PYR_c", "CIT_i", "CIT_c", "AKG_i",
-    #                                   "AKG_c", "SUC_i", "SUC_c", "MAL_i", "MAL_c",
-    #                                   "ASP_i", "ASP_c", "GLU_i", "GLU_c", "FUM_i",
-    #                                   "FUM_c", "ICIT_i", "ICIT_c", "GLC_c", "G6P_c",
-    #                                   "PCr_c", "AMP_c"])
-    # results.to_csv("../results/resultsIschemia.csv")
-    # feather.write_dataframe(results, "../results/resultsIschemia.feather")
+    pc.livFinalConditions[pc.pcIS.iPYR_c] = normPYR/10.0
+    pc.livFinalConditions[pc.pcIS.iO2_x] = normO2/10.0
+    #print(f(0, pc.finalConditions))
+
+    count = 1
+    results = sci.solve_ivp(fun = f,
+                            t_span = (0, 10000),
+                            y0 = pc.livFinalConditions,
+                            method = "LSODA",
+                            atol = 1e-10,
+                            rtol = 1e-10)
+
+    final = results.y.transpose()[-1]
+    results = np.concatenate((np.array([results.t]), results.y)).transpose()
+    results = pd.DataFrame(results,
+                           columns = ["t", "H_x", "dPsi", "ATP_x", "ADP_x",
+                                      "AMP_x", "GTP_x", "GDP_x", "Pi_x", "NADH_x",
+                                      "QH2_x", "OAA_x", "ACCOA_x", "CIT_x", "ICIT_x",
+                                      "AKG_x", "SCOA_x", "COASH_x", "SUC_x", "FUM_x",
+                                      "MAL_x", "GLU_x", "ASP_x", "K_x", "Mg_x",
+                                      "O2_x", "CO2tot_x", "Cred_i", "ATP_i", "ADP_i",
+                                      "AMP_i", "Pi_i", "H_i", "Mg_i", "K_i", "ATP_c",
+                                      "ADP_c", "Pi_c", "H_c", "Mg_c", "K_c", "PYR_x",
+                                      "PYR_i", "PYR_c", "CIT_i", "CIT_c", "AKG_i",
+                                      "AKG_c", "SUC_i", "SUC_c", "MAL_i", "MAL_c",
+                                      "ASP_i", "ASP_c", "GLU_i", "GLU_c", "FUM_i",
+                                      "FUM_c", "ICIT_i", "ICIT_c", "GLC_c", "G6P_c",
+                                      "PCr_c", "AMP_c"])
+    results.to_csv("../results/resultsIschemia.csv")
+    feather.write_dataframe(results, "../results/resultsIschemia.feather")
 
     if count == 0:
-        final = pd.read_feather("../results/resultsIschemia.feather")
-        final = np.delete(final.tail(1).to_numpy(), [0])
-
+        final = pd.read_csv("../results/resultsIschemia.csv")
+        final = np.delete(final.tail(1).to_numpy(), [0, 1])
+    #final[pc.pcIS.iSUC_x] = 20.0*pc.livFinalConditions[pc.pcIS.iSUC_x]
     finalIschemia = final
 
     # pc.finalConditions = final
@@ -400,7 +401,34 @@ def main(): ## Runs differential equation for time span and outputs results to
     pc.finalConditions[pc.pcIS.iO2_x] = normO2
     pc.finalConditions[pc.pcIS.iPYR_x] = normPYR
 
-    # results = sci.solve_ivp(fun=g,
+    results = sci.solve_ivp(fun=g,
+                            t_span=(0, 1000),
+                            y0=pc.finalConditions,
+                            method="LSODA",
+                            atol=1e-10,
+                            rtol=1e-10)
+    final = results.y.transpose()[-1]
+    results = np.concatenate((np.array([results.t]), results.y)).transpose()
+    results = pd.DataFrame(results,
+                           columns=["t", "H_x", "dPsi", "ATP_x", "ADP_x",
+                                    "AMP_x", "GTP_x", "GDP_x", "Pi_x", "NADH_x",
+                                    "QH2_x", "OAA_x", "ACCOA_x", "CIT_x", "ICIT_x",
+                                    "AKG_x", "SCOA_x", "COASH_x", "SUC_x", "FUM_x",
+                                    "MAL_x", "GLU_x", "ASP_x", "K_x", "Mg_x",
+                                    "O2_x", "CO2tot_x", "Cred_i", "ATP_i", "ADP_i",
+                                    "AMP_i", "Pi_i", "H_i", "Mg_i", "K_i", "ATP_c",
+                                    "ADP_c", "Pi_c", "H_c", "Mg_c", "K_c", "PYR_x",
+                                    "PYR_i", "PYR_c", "CIT_i", "CIT_c", "AKG_i",
+                                    "AKG_c", "SUC_i", "SUC_c", "MAL_i", "MAL_c",
+                                    "ASP_i", "ASP_c", "GLU_i", "GLU_c", "FUM_i",
+                                    "FUM_c", "ICIT_i", "ICIT_c", "GLC_c", "G6P_c",
+                                    "PCr_c", "AMP_c"])
+    feather.write_dataframe(results,
+                            "../results/resultsReperfusionOXPHOSPool12.feather")
+
+
+    ## Halved the reduction in OXPHOS
+    # results = sci.solve_ivp(fun=h,
     #                         t_span=(0, 1000),
     #                         y0=pc.finalConditions,
     #                         method="LSODA",
@@ -423,35 +451,8 @@ def main(): ## Runs differential equation for time span and outputs results to
     #                                 "FUM_c", "ICIT_i", "ICIT_c", "GLC_c", "G6P_c",
     #                                 "PCr_c", "AMP_c"])
     # feather.write_dataframe(results,
-    #                         "../results/resultsReperfusionOXPHOSPool12.feather")
-
-
-    ## Halved the reduction in OXPHOS
-    results = sci.solve_ivp(fun=h,
-                            t_span=(0, 1000),
-                            y0=pc.finalConditions,
-                            method="LSODA",
-                            atol=1e-10,
-                            rtol=1e-10)
-    final = results.y.transpose()[-1]
-    results = np.concatenate((np.array([results.t]), results.y)).transpose()
-    results = pd.DataFrame(results,
-                           columns=["t", "H_x", "dPsi", "ATP_x", "ADP_x",
-                                    "AMP_x", "GTP_x", "GDP_x", "Pi_x", "NADH_x",
-                                    "QH2_x", "OAA_x", "ACCOA_x", "CIT_x", "ICIT_x",
-                                    "AKG_x", "SCOA_x", "COASH_x", "SUC_x", "FUM_x",
-                                    "MAL_x", "GLU_x", "ASP_x", "K_x", "Mg_x",
-                                    "O2_x", "CO2tot_x", "Cred_i", "ATP_i", "ADP_i",
-                                    "AMP_i", "Pi_i", "H_i", "Mg_i", "K_i", "ATP_c",
-                                    "ADP_c", "Pi_c", "H_c", "Mg_c", "K_c", "PYR_x",
-                                    "PYR_i", "PYR_c", "CIT_i", "CIT_c", "AKG_i",
-                                    "AKG_c", "SUC_i", "SUC_c", "MAL_i", "MAL_c",
-                                    "ASP_i", "ASP_c", "GLU_i", "GLU_c", "FUM_i",
-                                    "FUM_c", "ICIT_i", "ICIT_c", "GLC_c", "G6P_c",
-                                    "PCr_c", "AMP_c"])
-    #feather.write_dataframe(results,
-    #            "../results/resultsReperfusionHalvedOXPHOSPool12.feather")
-    results.to_csv("../results/resultsReperfusionHalvedOXPHOSPool12.csv")
+    #             "../results/resultsReperfusionHalvedOXPHOSPool12.feather")
+    # results.to_csv("../results/resultsReperfusionHalvedOXPHOSPool12.csv")
 
 
 #start = time.time()
